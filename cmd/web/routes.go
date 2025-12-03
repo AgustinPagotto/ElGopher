7adf18814df4497fc10d22419c14c3198dfdf98f
AgustinPagotto <agustinpagotto@gmail.com>
2025-12-03 17:23:41 -0300

    add static folder, add css bare file, add /static to load styles

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,6 +4,8 @@ import "net/http"
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/{$}", home)
 	mux.HandleFunc("/about", app.about)
 	mux.HandleFunc("/article/create", articleCreate)
