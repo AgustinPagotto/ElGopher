1865b6b9f530af631c6e6e97de824c2d639f52ba
AgustinPagotto <agustinpagotto@gmail.com>
2025-12-03 16:00:42 -0300

    add ui/html/page, an about page and an about handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello"))
+}
+func (a *application) about(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("./ui/html/pages/about.html")
+	if err != nil {
+		a.logger.Error(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		a.logger.Error(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 func articleCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("articleCreate"))
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,6 +5,7 @@ import "net/http"
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/about", app.about)
 	mux.HandleFunc("/article/create", articleCreate)
 	mux.HandleFunc("/article/view/{id}", articleView)
 	return mux
