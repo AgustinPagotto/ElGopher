6c3a6e0c0756dcbd25728c6cc45d1c2720128006
AgustinPagotto <agustinpagotto@gmail.com>
2025-12-02 22:18:35 -0300

    add routes file and move routes there

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,16 +12,12 @@ type application struct {
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/article/create", articleCreate)
-	mux.HandleFunc("/article/view/{id}", articleView)
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
 	app := &application{
 		logger: logger,
 	}
 	app.logger.Info("Starting server at :4000")
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(":4000", app.routes())
 	if err != nil {
 		fmt.Print(err)
 	}
