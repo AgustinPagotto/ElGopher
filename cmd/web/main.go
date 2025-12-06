1ff8e8fc24348e08acae72d11c49e70d89ff9dba
AgustinPagotto <agustinpagotto@gmail.com>
2025-12-06 20:55:28 -0300

    add driver for postgres data base, and add the pool to the rest of the code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,35 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/AgustinPagotto/ElGopher/internal/models"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type application struct {
 	logger        *slog.Logger
 	templateCache map[string]*template.Template
+	articles      models.ArticleModelInterface
 }
 
 func main() {
+	dsn := flag.String("dsn", "postgres://postgres:admin@localhost:5432/gotodo", "String connection for PostgreSql")
+	flag.Parse()
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
 	templateCache, err := newTemplateCache()
 	if err != nil {
 		logger.Error(err.Error())
@@ -24,6 +38,7 @@ func main() {
 	app := &application{
 		logger:        logger,
 		templateCache: templateCache,
+		articles:      &models.ArticleModel{DB: db},
 	}
 	srv := &http.Server{
 		Addr:         ":4000",
@@ -38,4 +53,19 @@ func main() {
 	if err != nil {
 		fmt.Print(err)
 	}
+}
+
+func openDB(dsn string) (*pgxpool.Pool, error) {
+	var err error
+	var ctx = context.Background()
+	pool, err := pgxpool.New(ctx, dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, err
+	}
+	return pool, nil
 }
