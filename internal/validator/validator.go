0c3ca4cd1888c1dbc3c563c9773cd6361074da15
AgustinPagotto <agustinpagotto@gmail.com>
2025-12-24 12:12:16 -0300

    add validators, add things to handler, delete commented code in about.html

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,9 +1,12 @@
 package validator
 
 import (
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 type Validator struct {
 	NonFieldErrors []string
@@ -43,4 +46,16 @@ func MaxChars(value string, n int) bool {
 
 func MinChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) >= n
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+func EmailValidator(value string) bool {
+	return NotBlank(value) && Matches(value, EmailRX)
+}
+
+func PasswordValidator(value string) bool {
+	return NotBlank(value) && MinChars(value, 8)
 }
