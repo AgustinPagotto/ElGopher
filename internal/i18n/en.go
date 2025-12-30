9311c0ef466d6c41863d4091d43028cd058b2726
AgustinPagotto <agustinpagotto@gmail.com>
2025-12-30 12:16:02 -0300

    add translations and add new humandate as dd/mm/yyyy

--- internal/i18n/en.go
+++ internal/i18n/en.go
@@ -1,9 +1,10 @@
 package i18n
 
 var EN = map[string]string{
-	"nav.home":     "Home",
-	"nav.articles": "Articles",
-	"nav.projects": "Projects",
-	"nav.about":    "About",
-	"nav.logout":   "Logout",
+	"nav.home":        "Home",
+	"nav.articles":    "Articles",
+	"nav.projects":    "Projects",
+	"nav.about":       "About",
+	"nav.logout":      "Logout",
+	"articles.header": "El Gopher Articles",
 }
