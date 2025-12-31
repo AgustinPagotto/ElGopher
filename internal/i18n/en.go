d5a633d5bc3e2bca28e80788cd0866a85f97f9ee
AgustinPagotto <agustinpagotto@gmail.com>
2025-12-31 12:22:52 -0300

    add function to templates to have line breaks, add all translations to home page

--- internal/i18n/en.go
+++ internal/i18n/en.go
@@ -1,10 +1,19 @@
 package i18n
 
 var EN = map[string]string{
+	"blog":            "Blog",
 	"nav.home":        "Home",
-	"nav.articles":    "Articles",
 	"nav.projects":    "Projects",
 	"nav.about":       "About",
 	"nav.logout":      "Logout",
 	"articles.header": "El Gopher Articles",
+	"home.header":     "Welcome",
+	"home.whoami":     "Who am I?",
+	"home.whoamiparagraph": `Hello! I'm Agustin!
+I work remotely as a Software Engineer at Teladoc Health. Previously, I worked at Tsoft for 2 years.
+I have a barchelors degree en System Engineering and I live in a small city in the north of Argentina.`,
+	"home.blogparagraph":       `This website has the purpose of introducing myself, and also to be a place to write about technical and not so technical stuff about Go.`,
+	"home.gotolastarticlelink": "Go to my latest article -> ",
+	"home.contact":             "Contact",
+	"home.contactparagraph":    "If you have a doubt about the site, have a job oportunity that i might be interested in or you just want to talk about programming or go, don't hesitate to reach out!",
 }
