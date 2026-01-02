9566de147455720c58d2151c438453794290b63a
AgustinPagotto <agustinpagotto@gmail.com>
2026-01-02 18:17:14 -0300

    fix some text errors

--- internal/i18n/es.go
+++ internal/i18n/es.go
@@ -9,20 +9,20 @@ var ES = map[string]string{
 	"articles.header": "El Gopher Articulos",
 	"home.header":     "Bienvenido",
 	"home.whoami":     "Quien Soy?",
-	"home.whoamiparagraph": `Hola! Soy Agustin 
-Trabajo de manera remota como Software Engineer en Teladoc Health. Previamente, trabaje en TSOFT por 2 años. 
-Vivo en La Rioja, Argentina y soy Ingeniero en Sistemas.`,
-	"home.blogparagraph":       `El proposito de este sitio es introducirme formalmente, y tambien tener un espacio personal para escribir acerca de Go de forma tecnica y no tan tecnica.`,
+	"home.whoamiparagraph": `Hola! Soy Agustin, Ingeniero en Sistemas y Software Engineer.
+	Trabajo de manera remota como Software Engineer en Teladoc Health. Previamente, trabaje en TSOFT por 2 años. 
+	Vivo en La Rioja, Argentina.`,
+	"home.blogparagraph":       `El proposito de este sitio es introducirme formalmente, como tambien el de ser un espacio personal para escribir acerca de Go de forma tecnica y no tan tecnica.`,
 	"home.gotolastarticlelink": "Ir a mi ultimo articulo ->",
 	"home.contact":             "Contacto",
 	"home.contactparagraph":    "Si tienes alguna duda acerca del sitio, algun articulo, o me quieres contactar por algun proyecto interesante. O para hablar de GO, o programación en general. No dudes en contactarme!.",
 	"about.techstack":          "ElGopher Page Tech Stack",
-	"about.gotechstack":        " para la logica del backend and templates",
-	"about.htmxtechstack":      " para las interacciones con el usuario",
-	"about.psqltechstack":      " para base de datos",
-	"about.picotechstack":      " framework liviano para stilos",
+	"about.gotechstack":        " para la logica del backend y templates",
+	"about.htmxtechstack":      " frontend para interacciones dinamicas con el usuario",
+	"about.psqltechstack":      " base de datos",
+	"about.picotechstack":      " framework de estilos",
 	"about.thanks":             "Gracias!",
-	"about.thanksparagraph": `En esta seccion voy a dar las gracias a las personas/autores que dejaron que aprenda con sus libros, podcasts, articulos, etc. e hicieron mi camino a desarrollar en Go posible.
+	"about.thanksparagraph": `En esta seccion voy a dar las gracias a las personas/autores que dejaron que aprenda de sus libros, podcasts y articulos, e hicieron mi camino de aprender y desarrollar en Go posible y mas ameno.
 	Muchas gracias a:`,
 	"projects.personalprojects": "Proyectos Personales",
 }
