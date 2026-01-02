fe0e0e4a81da4a695dc1ff0ec687b9820e2aa212
AgustinPagotto <agustinpagotto@gmail.com>
2026-01-02 16:06:51 -0300

    add translations for the about page

--- internal/i18n/en.go
+++ internal/i18n/en.go
@@ -2,9 +2,9 @@ package i18n
 
 var EN = map[string]string{
 	"blog":            "Blog",
+	"about":           "About",
 	"nav.home":        "Home",
 	"nav.projects":    "Projects",
-	"nav.about":       "About",
 	"nav.logout":      "Logout",
 	"articles.header": "El Gopher Articles",
 	"home.header":     "Welcome",
@@ -16,4 +16,12 @@ I have a barchelors degree en System Engineering and I live in a small city in t
 	"home.gotolastarticlelink": "Go to my latest article -> ",
 	"home.contact":             "Contact",
 	"home.contactparagraph":    "If you have a doubt about the site, have a job oportunity that i might be interested in or you just want to talk about programming or go, don't hesitate to reach out!",
+	"about.techstack":          "ElGopher Page Tech Stack",
+	"about.gotechstack":        " for backend logic and templating",
+	"about.htmxtechstack":      " for lightweight interactions",
+	"about.psqltechstack":      " for persistence",
+	"about.picotechstack":      " for lightweight styling",
+	"about.thanks":             "Thanks!",
+	"about.thanksparagraph": ` This section will be just to give thanks to all the authors/people that let me learn from his books, podcasts, articles, etc. and made my journey to being a Go developer possible.
+	Thanks a lot to:`,
 }
--- internal/i18n/es.go
+++ internal/i18n/es.go
@@ -2,9 +2,9 @@ package i18n
 
 var ES = map[string]string{
 	"blog":            "Blog",
+	"about":           "About",
 	"nav.home":        "Inicio",
 	"nav.projects":    "Projectos",
-	"nav.about":       "Acerca",
 	"nav.logout":      "Desloguear",
 	"articles.header": "El Gopher Articulos",
 	"home.header":     "Bienvenido",
@@ -16,4 +16,12 @@ Vivo en La Rioja, Argentina y soy Ingeniero en Sistemas.`,
 	"home.gotolastarticlelink": "Ir a mi ultimo articulo ->",
 	"home.contact":             "Contacto",
 	"home.contactparagraph":    "Si tienes alguna duda acerca del sitio, algun articulo, o me quieres contactar por algun proyecto interesante. O para hablar de GO, o programación en general. No dudes en contactarme!.",
+	"about.techstack":          "ElGopher Page Tech Stack",
+	"about.gotechstack":        " para la logica del backend and templates",
+	"about.htmxtechstack":      " para las interacciones con el usuario",
+	"about.psqltechstack":      " para base de datos",
+	"about.picotechstack":      " framework liviano para stilos",
+	"about.thanks":             "Gracias!",
+	"about.thanksparagraph": `En esta seccion voy a dar las gracias a las personas/autores que dejaron que aprenda con sus libros, podcasts, articulos, etc. e hicieron mi camino a desarrollar en Go posible.
+	Muchas gracias a:`,
 }
