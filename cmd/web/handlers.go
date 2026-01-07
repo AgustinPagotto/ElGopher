740ae6cc2b34b5eeeea29077116ca137cb0dd8e2
AgustinPagotto <agustinpagotto@gmail.com>
2026-01-07 12:38:07 -0300

    add ping, add testutils to test end-to-end, add assert to make testing easier, add basic middleware and handler tests as base for the others, add template test for basic functions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -30,6 +30,10 @@ type logInForm struct {
 	validator.Validator `form:"-"`
 }
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, http.StatusOK, "home.html", app.newTemplateData(r))
 }
---
+++ cmd/web/templates_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/AgustinPagotto/ElGopher/internal/assert"
+)
+
+func TestHumanDate(t *testing.T) {
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2024, 3, 12, 15, 0, 0, 0, time.UTC),
+			want: "12/03/2024",
+		},
+		{
+			name: "empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2024, 3, 12, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "12/03/2024",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hd := humanDate(tt.tm)
+			assert.Equal(t, hd, tt.want)
+		})
+	}
+}
