b69c8802a7ae37f9c6893df1faa403256bf19057
AgustinPagotto <agustinpagotto@gmail.com>
2026-01-18 21:04:59 -0300

    add testing of article view

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -56,3 +56,52 @@ func TestSetLightMode(t *testing.T) {
 	status, _, body := ts.get(t, "/")
 	assert.StringContains(t, string(body), "light")
 }
+
+func TestArticleView(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody string
+	}{
+		{
+			name:     "Latest Article",
+			urlPath:  "/article/view/latest",
+			wantCode: http.StatusOK,
+			wantBody: "An old silent pond",
+		},
+		{
+			name:     "Existent Article",
+			urlPath:  "/article/view/an-old-silent-pond",
+			wantCode: http.StatusOK,
+			wantBody: "An old silent pond",
+		},
+		{
+			name:     "Non Existent Slug",
+			urlPath:  "/article/view/asdf-asdf",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Number Slug",
+			urlPath:  "/article/view/123-123",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Empty Slug",
+			urlPath:  "/article/view/",
+			wantCode: http.StatusNotFound,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.get(t, tt.urlPath)
+			assert.Equal(t, tt.wantCode, code)
+			if tt.wantBody != "" {
+				assert.StringContains(t, body, tt.wantBody)
+			}
+		})
+	}
+}
--- internal/models/mocks/articles.go
+++ internal/models/mocks/articles.go
@@ -2,15 +2,23 @@ package mocks
 
 import (
 	"context"
+	"time"
 
 	"github.com/AgustinPagotto/ElGopher/internal/models"
 )
 
 var mockArticle = models.Article{
-	ID:          1,
-	Title:       "An old silent pond",
-	Body:        "An old silent pond...",
+	ID:    1,
+	Title: "An old silent pond",
+	Body: `
+		# An old silent pond
+		This is a short test article written in **Markdown**.
+	`,
+	Slug:        "an-old-silent-pond",
+	Excerpt:     "An old silent pond...",
 	IsPublished: false,
+	Created:     time.Now(),
+	UpdatedAt:   time.Now(),
 }
 
 type ArticleModel struct{}
@@ -34,7 +42,12 @@ func (am *ArticleModel) Update(ctx context.Context, title, body string, is_publi
 	return nil
 }
 func (am *ArticleModel) GetWithSlug(ctx context.Context, slug string) (models.Article, error) {
-	return mockArticle, nil
+	switch slug {
+	case "an-old-silent-pond":
+		return mockArticle, nil
+	default:
+		return models.Article{}, models.ErrNoRecord
+	}
 }
 func (am *ArticleModel) GetArticles(ctx context.Context) ([]models.Article, error) {
 	return []models.Article{}, nil
